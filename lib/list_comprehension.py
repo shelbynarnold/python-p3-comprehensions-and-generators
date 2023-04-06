d7ac50e25f284f945e4b3485aa97dbb68ab4bfcb
shelby <shelbynarnold@yahoo.com>
2023-04-06 10:10:01 -0600

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_list = [i for i in num_list if i %2 == 0]
+    return new_list
+    # for i in range(0,8,2):
+    #     return [0, 2, 4, 6, 8]
+    # else:
+    #     return [] 
 
 def make_exclamation(sentence_list):
-    pass
+    new_list =[sentence + "!" for sentence in sentence_list if sentence != ""]
+    return new_list
+
+
+
+ 
